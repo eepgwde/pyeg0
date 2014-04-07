5256762e964248742f82d88218c557d7c22cb77a
Walter Eaves <walter.d.eaves@gmail.com>
2014-04-07 15:49:23 +0100

    -

---
+++ gmus/test0.py
@@ -0,0 +1,16 @@
+from gmusicapi import Mobileclient
+import ConfigParser, os
+
+cfg = ConfigParser.ConfigParser()
+
+cfg.read(['site.cfg', os.path.expanduser('~/etc/gmusic.cfg')])
+
+api = Mobileclient()
+
+api.login(cfg.get('credentials', 'username'),
+          cfg.get('credentials', 'password'))
+
+s0 = api.get_all_songs()
+
+print len(s0)
+
