952c07cf99906478084163cc10392d5d93fe55b8
Walter Eaves <walter.d.eaves@gmail.com>
2014-07-25 16:11:52 +0100

    -

--- bt0/Bt1.py
+++ bt0/Bt1.py
@@ -15,13 +15,9 @@
 from __future__ import print_function
 
 import logging
-import ConfigParser, os, logging
+from os.path import basename
 
 from Bt0 import Bt0
-
-from io import StringIO
-
-import json
 
 ## BitTorrent
 #
@@ -33,40 +29,14 @@ class Bt1(Bt0):
     """
 
     def __init__(self, file0):
-        super(Child, self).__init__(file0)
+        super(Bt1, self).__init__(file0)
         return
 
     def display(self):
         """
         XML output
         """
-        print('metainfo file.: %s' % basename(file0))
-        print('info hash.....: %s' % info_hash.hexdigest())
-        piece_length = info['piece length']
-        if info.has_key('length'):
-            # let's assume we just have a file
-            print('file name.....: %s' % info['name'])
-            file_length = info['length']
-            name ='file size.....:'
-            return
-
-        # let's assume we have a directory structure
-        print('directory name: %s' % info['name'])
-        print('files.........: ')
-        file_length = 0;
-        for file in info['files']:
-            path = ''
-            for item in file['path']:
-                if (path != ''):
-                    path = path + "/"
-                    path = path + item
-                    print('   %s (%d)' % (path, file['length']))
-                    file_length += file['length']
-                    name = 'archive size..:'
-                    piece_number, last_piece_length = divmod(file_length, piece_length)
-                    print('%s %i (%i * %i + %i)' % (name,file_length, piece_number, piece_length, last_piece_length))
-                    print('announce url..: %s' % announce)
-                    print
+        print('XML metainfo file.: %s' % basename(self.file0))
         return
 
     def dispose(self):
