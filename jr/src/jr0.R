c5c4652d9d8b212714c6d0a8f04c152368ae52db
Walter Eaves <walter.d.eaves@gmail.com>
2016-05-09 16:42:40 +0100

    -

--- jr/src/jr0.R
+++ jr/src/jr0.R
@@ -26,6 +26,16 @@ folios.in <- folios.df[folios.df$in0, c(names.base, names.x)]
 
 folios.in0 <- tail(folios.in, n=40)
 
+## Quick time-series plot
+
+names.xr <- append(names.x, "r00")
+
+ts.plot(folios.in0[, names.xr],
+             gpars=list(xlab="day", ylab="metric", 
+                        lty=c(1:length(names.xr)) ))
+legend("bottomleft", names.xr, col=1:length(names.xr), lty=, cex=.65)
+
+
 ## A short set
 
 folios.df1 <- tail(folios.df, n=40)
