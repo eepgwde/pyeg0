2a7f32cc688c8331e5c1285dc5ae0d08dec62cd9
Walter Eaves <walter.d.eaves@gmail.com>
2016-07-24 03:42:46 +0100

    added support for demographic extensions

--- pru/src/pru3.R
+++ pru/src/pru3.R
@@ -54,22 +54,26 @@ x0 <- unstack(x0, x2tp ~ Categories)
 
 x.wdi <- TRUE
 x.demog <- FALSE                        # and do not zero fill
+x.price <- FALSE                        # and do not zero fill
 
 ## This is how to switch them off
 ## rm(x.wdi)
 ## rm(x.demog)
+## rm(x.price)
 
 if (exists("x.wdi")) {
 
     ## I'll just use the deltas and fill-back.
 
     ## @note
-    ## The demographic data doesn't help much - might be better for deciles?
+    ## The demographic data does help, halves my var1 estimate below.
+    ## Must fill forward and back.
     if (exists("x.demog")) {
+
         ## Some demographic data, added as deltas, this is relatively
         ## stable and we can fill forward
         ## @note
-        ## It doesn't help at all!
+        ## These are not so important.
         m0 <- c("SP.ADO.TFRT","SP.DYN.TFRT.IN")
         x1 <- wdi.filled(wdi0=wdi$demog$values, metrics0=m0, zero0=x.demog)
 
@@ -79,7 +83,50 @@ if (exists("x.wdi")) {
         x0 <- cbind(x0, x1)                 # add the columns
 
         ## Some more about male/female in population
-        ## This isn't very complete
+        ## These are important.
+        ## The data isn't very complete.
+        m0 <- c("SP.POP.TOTL", "SP.POP.TOTL.FE.ZS", "SL.EMP.TOTL.SP.FE.ZS", "SL.EMP.TOTL.SP.MA.ZS", "SL.EMP.TOTL.SP.ZS")
+        x1 <- wdi.filled(wdi0=wdi$popn$values, metrics0=m0, zero0=x.demog)
+
+        stopifnot(nrow(x0) == nrow(x1))
+        x1$year <- NULL                     
+
+        x0 <- cbind(x0, x1)
+    }
+
+    ## @note
+    ## The pricing data makes no change
+    if (exists("x.price")) {
+
+        ## CPI and Wholesale, wholesale is a very short set, so I've set it equal to CPI.
+        m0 <- c("CPTOTSAXN", "FP.WPI.TOTL")
+        x1 <- wdi.filled(wdi0=wdi$price$values, metrics0=m0, zero0=x.price)
+        x1[((nrow(x1)-2):nrow(x1)),2] <- x1[((nrow(x1)-2):nrow(x1)),1] # short set kludge
+
+        stopifnot(nrow(x0) == nrow(x1))
+        x1$year <- NULL                     # I kept the year in for debugging.
+
+        x0 <- cbind(x0, x1)                 # add the columns
+    }
+
+    ## @note
+    ## The demographic data doesn't help much - might be better for deciles?
+    if (exists("x.price")) {
+
+        ## Some demographic data, added as deltas, this is relatively
+        ## stable and we can fill forward
+        ## @note
+        ## These are not so important.
+        m0 <- c("SP.ADO.TFRT","SP.DYN.TFRT.IN")
+        x1 <- wdi.filled(wdi0=wdi$demog$values, metrics0=m0, zero0=x.demog)
+
+        stopifnot(nrow(x0) == nrow(x1))
+        x1$year <- NULL                     # I kept the year in for debugging.
+
+        x0 <- cbind(x0, x1)                 # add the columns
+
+        ## Some more about male/female in population
+        ## This isn't very complete data, but does improve accuracy.
         m0 <- c("SP.POP.TOTL", "SP.POP.TOTL.FE.ZS", "SL.EMP.TOTL.SP.FE.ZS", "SL.EMP.TOTL.SP.MA.ZS", "SL.EMP.TOTL.SP.ZS")
         x1 <- wdi.filled(wdi0=wdi$popn$values, metrics0=m0, zero0=x.demog)
 
