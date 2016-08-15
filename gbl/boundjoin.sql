846a65362eb7f7f3dc2d53238114b34ab81f3853
Walter Eaves <Walter.D.Eaves@gmail.com>
2016-08-15 16:59:12 +0100

    -

--- gbl/boundjoin.sql
+++ gbl/boundjoin.sql
@@ -24,9 +24,8 @@ from cmpl2 c;
 
 drop view vwCmpl20;
 create view vwCmpl20 as select c.source_complaint_id id
-, date_add(c.date_created, interval 2 day) as 'udt'
-, c.date_created as 'dt'
-, date_sub(c.date_created, interval 2 day) as 'ldt'
+, date_add(c.date_created, interval 7 day) as 'udt'
+, date_sub(c.date_created, interval 7 day) as 'ldt'
 , (1+0.05) * v.amount1 as v1u
 , v.amount1 as v1
 , (1-0.05) * v.amount1 as v1l
@@ -38,8 +37,9 @@ select c2.party_id, t.event_id, c2.source_complaint_id
 , t.start_dt
 , v0.ldt
 , t.amount0
-, c2.Comment_tx
-from trans t left join cmpl2 c2 on c2.party_id = t.party_id
-  left join vwCmpl20 v0 on c2.source_complaint_id = v0.id
+, v0.v1
+from trans t inner join cmpl2 c2 on c2.party_id = t.party_id
+  right join vwCmpl20 v0 on c2.source_complaint_id = v0.id
   and t.start_dt <= v0.udt and t.start_dt >= v0.ldt
-  and t.amount0 <= v0.v1u and t.amount0 >= v0.v1l ;
+  and t.amount0 <= v0.v1u and t.amount0 >= v0.v1l
+order by c2.source_complaint_id;
