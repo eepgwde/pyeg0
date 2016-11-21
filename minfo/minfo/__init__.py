1a95df7be7493e3fc6b7beeb769c45f726d2c467
Walter Eaves <Walter.D.Eaves@gmail.com>
2016-11-21 15:41:49 +0000

    -

---
+++ minfo/minfo/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from minfo import MInfo, MInfo1
+
+__copyright__ = 'Copyright 2016 Walter Eaves'
+__license__ = 'GPLv3'
+__title__ = 'minfo'
+__version__ = '0.1.0'
+
+# appease flake8: the imports are purposeful
+(__version__, MInfo, MInfo1)
