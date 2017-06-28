6a71ebef3f6b1caebc68f383427951ba30c114ff
Walter Eaves <walter.d.eaves@gmail.com>
2017-06-28 18:34:45 +0100

    trying to fix it.

--- flights/src/mutagen.R
+++ flights/src/mutagen.R
@@ -2,6 +2,12 @@
 
 ## Following mutagen in Building Predictive Models in R Using the caret Package
 
+## I can't get past the correlations highCorr. NA elements.
+
+library(QSARdata)
+
+data(Mutagen)
+
 set.seed(1)
 
 mutagen <- Mutagen_Outcome
@@ -19,6 +25,37 @@ prop.table(table(mutagen))
 prop.table(table(trainClass))
 
 ncol(trainDescr)
+
+nzv <- nearZeroVar(trainDescr, saveMetrics= TRUE)
+
+nzv1 <- nzv[ nzv$nzv, ]
+nzv1 <- nzv1[ order(nzv1$percentUnique, nzv1$freqRatio), ]
+
+nnames <- setdiff(colnames(trainDescr), rownames(nzv)[nzv$zeroVar])
+
+descr <- Mutagen_Dragon[,nnames]
+
+inTrain <- createDataPartition(mutagen, p = 3/4, list = FALSE)
+
+trainDescr <- descr[inTrain,]
+testDescr <- descr[-inTrain,]
+trainClass <- mutagen[inTrain]
+testClass <- mutagen[-inTrain]
+
+prop.table(table(mutagen))
+
+prop.table(table(trainClass))
+
+ncol(trainDescr)
+
+descrCorr <- cor(scale(trainDescr), use = "pairwise.complete.obs")
+
+cols <- names(which(is.na(df[1,])))
+nnames <- setdiff(colnames(trainDescr), cols)
+
+trainDescr <- trainDescr[, nnames]
+
+
 
 descrCorr <- cor(scale(trainDescr), use = "pairwise.complete.obs")
 
