c54e6294c63c8cec07ce6f81e7b7e1611ae2110b
Walter Eaves <walter.d.eaves@gmail.com>
2017-07-11 19:16:43 +0100

    -

--- codeassess/try.c
+++ codeassess/try.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+int max0(int a, int b) {
+  if (a>b) return 1;
+  if (a<b) return -1;
+  return 0;
+}
+  
 static int* a0;
 static int N0;
 static int M0;
@@ -15,59 +21,89 @@ void write0(pair_t p) {
   printf("row: %d; col: %d\n", p.r, p.c);
 }
 
-int value0(int r, int c) {
-  return *(a0 + N0*r + c);
+int value0(int *arr, int r, int c) {
+  return *(arr + N0*r + c);
 }
 
-int value1(pair_t p) {
+int value1(int *arr, pair_t p) {
   // printf("p.r %d; p.c %d; %d\n", p.r, p.c, *(a0 + N0*p.r + p.c));
-  return *(a0 + N0*p.r + p.c);
+  return value0(arr, p.r, p.c);
+}
+
+pair_t toleft(int r, int c) {
+  if ((c-1) < 0)
+    return fail0;
+
+  pair_t p;
+  p.r = r;
+  p.c = c-1;
+  return p;
+}
+
+pair_t toabove(int r, int c) {
+  if ((r-1) < 0)
+    return fail0;
+
+  pair_t p;
+  p.r = r-1;
+  p.c = c;
+  return p;
+}
+
+int left0(int *arr, pair_t p) {
+  pair_t r0 = toleft(p.r, p.c);
+
+  if (r0.r >= 0)
+    if (value1(arr, r0) < value1(arr, p)) return 1;
+
+  return 0;
+}
+
+int above0(int *arr, pair_t p) {
+  pair_t r0 = toabove(p.r, p.c);
+
+  if (r0.r >= 0)
+    if (value1(arr, r0) < value1(arr, p)) return 1;
+
+  return 0;
 }
 
 /*
 
 Need a (recursive) back-tracking algorithm.
 
-Or some other method.
+try(here) {
+
+if hasright(here) && value(right) > value(here)
+then cand1 = right
+
+if hasbelow(here) && value(below) > value(here)
+then cand2 = below
+
+count1 = try(right)
+count2 = try(below)
+
+return max0(count1, count2)
+
+}
 
 */
 
-pair_t toright(int r, int c) {
-  if ((c+1) >= M0)
-    return fail0;
+int try(pair_t p, int count) {
 
-  pair_t p;
-  p.r = r;
-  p.c = c+1;
-  return p;
+  
+  return max0(count1, count2);
 }
 
-pair_t todown(int r, int c) {
-  if ((r+1) >= N0)
-    return fail0;
-
-  pair_t p;
-  p.r = r+1;
-  p.c = c;
-  return p;
-}
-
-int right0(pair_t p) {
-  pair_t r0 = toright(p.r, p.c);
-
-  if (r0.r >= 0)
-    if (value1(r0) > value1(p)) return 1;
-
-  return 0;
-}
-
-int down0(pair_t p) {
-  pair_t r0 = todown(p.r, p.c);
-
-  if (r0.r >= 0)
-    if (value1(r0) > value1(p)) return 1;
-
-  return 0;
+void write1(int *arr) {
+  int i, j;
+  
+  for(i=0; i<=N0-1; i++) {
+    for(j=0;j<=M0-1;j++) {
+      printf("%d ", value0(arr, i, j));
+    }
+    printf("\n");
+  }
 }
 
 int main () 
@@ -93,39 +129,39 @@ int main ()
       scanf("%d", &a[i][j]);
   }
 
-  write0(toright(0, 0));
+  write0(toleft(0, 0));
 
   /* output the array */
   for(i=0; i<=N-1; i++) {
     for(j=0;j<=M-1;j++) {
-      printf("%d - ", a[i][j]);
-      printf("value0 %d ; ", value0(i,j));
-      printf("toright %d ; ", toright(i,j).c);
-      printf("todown %d ; ", todown(i,j).r);
+      printf("%d ", a[i][j]);
     }
     printf("\n");
   }
 
-  printf("\n");
+  write1(a0);
+
+  int left[N][M];
   
   for(i=0; i<=N-1; i++) {
     for(j=0;j<=M-1;j++) {
       pair_t p = { i, j };
-      printf("%d ", right0(p));
+      left[i][j] = left0(a0, p);
     }
-    printf("\n");
   }
+
+  write1((int *)left);
  
-  /* go down */
-  printf("\n");
+  int above[N][M];
   
   for(i=0; i<=N-1; i++) {
     for(j=0;j<=M-1;j++) {
       pair_t p = { i, j };
-      printf("%d ", down0(p));
+      above[i][j] = above0(a0, p);
     }
-    printf("\n");
   }
  
+  write1((int *)above);
+
   return(0); 
 } 
