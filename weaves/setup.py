1f7a68776de09c6c26c12c423c4d7ab87ed0d608
Walter Eaves <Napoleon.1er.Bonaparte@gmail.com>
2018-11-11 17:43:01 +0000

    -

--- weaves/setup.py
+++ weaves/setup.py
@@ -4,5 +4,10 @@ from setuptools import setup
 
 setup(
     setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    install_requires=[
+          'markdown',
+          'unidecode',
+          'cached-property',
+    ],
     pbr=True,
 )
