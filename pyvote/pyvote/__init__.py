c99178a9b229eafabbc337d7bb018612b4cf042c
Walter Eaves <Napoleon.1er.Bonaparte@gmail.com>
2018-11-16 11:12:33 +0000

    -

--- pyvote/pyvote/__init__.py
+++ pyvote/pyvote/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from weaves._version import __version__, __Id__
-from weaves._Vote import Singleton as VoteOps
+from pyvote._version import __version__, __Id__
+from pyvote._Vote import Singleton as VoteOps
 
 __copyright__ = 'Copyright 2016 Walter Eaves'
 __license__ = 'GPLv3'
