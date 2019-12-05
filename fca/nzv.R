b3d6a0a6d7bdc533be191ca7bdf86cab54686418
Walter Eaves <walter.d.eaves@gmail.com>
2019-12-05 10:45:19 +0000

    refined some columns

---
+++ fca/nzv.R
@@ -0,0 +1,13 @@
+library(caret)
+
+df2 <- read.csv("coded.csv")
+
+freqCut0 <- 4
+unique0 <- 25
+
+nzv0 <- nearZeroVar(df2, saveMetrics = TRUE, allowParallel=TRUE,
+                    freqCut =freqCut0, 
+                    uniqueCut=unique0)
+
+
+plot(df2)
