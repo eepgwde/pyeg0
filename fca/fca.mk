2935842601be819dd98a210d2d46994c3bad7279
Walter Eaves <Napoleon.1er.Bonaparte@gmail.com>
2019-12-07 15:59:20 +0000

    peppy

--- fca/fca.mk
+++ fca/fca.mk
@@ -1,18 +1,30 @@
 X_OUT = cache/out
 X_SRC = $(HOME)/Documents
 
-.PHONY: view notebooks
+.PHONY: view notebooks dir0 dir1
 
-
-NBS := $(wildcard cpta*.ipynb)
+NBS := $(wildcard fca?.ipynb)
 NBS0 := $(NBS:.ipynb=.pdf)
 
-NBXL := $(wildcard $(X_SRC)/cpta*.xlsx)
+NBXL := $(wildcard $(X_SRC)/fca?.xlsx)
 NBXL0 = $(addprefix $(X_OUT)/,$(notdir $(NBXL)))
 
-TFILE := $(PWD)/cpta.zip
+TFILE := $(PWD)/fca.zip
 
-all:: notebooks $(NBXL0)
+# $(NBXL0)
+all:: dir1 notebooks
+
+dir0:
+	test -d cache || mkdir cache
+	test -d cache/bak || ( cd cache ; ln -s /cache/incoming/fca bak )
+
+all-local:: dir0 in.csv
+
+in.csv: 
+	ln -s cache/bak/bank-direct-marketing.csv in.csv
+
+dir1:
+	test -d $(X_OUT) || mkdir -p $(X_OUT)
 
 view:
 	-echo $(NBS0)
